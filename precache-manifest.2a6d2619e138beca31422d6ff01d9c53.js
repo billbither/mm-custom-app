a01c6b37338e7264940faf77d9befa2517d73794
billbither <billbither@gmail.com>
2020-05-27 17:17:27 -0400

    Add files via upload

---
+++ precache-manifest.2a6d2619e138beca31422d6ff01d9c53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5492cb3a001da0115eb81d6fde50ef67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e18b3b3f070fa8c19bc3",
+    "url": "/static/css/2.f22687d4.chunk.css"
+  },
+  {
+    "revision": "add2a1109922b4404ef2",
+    "url": "/static/css/main.7dc2c717.chunk.css"
+  },
+  {
+    "revision": "e18b3b3f070fa8c19bc3",
+    "url": "/static/js/2.07bdd9b8.chunk.js"
+  },
+  {
+    "revision": "111d9dd0d4f1e21fb19389ec92e1fa72",
+    "url": "/static/js/2.07bdd9b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add2a1109922b4404ef2",
+    "url": "/static/js/main.5132456b.chunk.js"
+  },
+  {
+    "revision": "83a0bae63143e1d95f58",
+    "url": "/static/js/runtime-main.248d202e.js"
+  }
+]);
